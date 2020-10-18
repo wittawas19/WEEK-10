a9bde205459930b014a2bcf65a89e1681289b798
wittawas19@gmail.com <wittawas19@gmail.com>
2020-10-18 16:49:15 +0700

    Add project files.

---
+++ WEEK 10/Source.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+void main()  
+{
+	char str[1000];
+	scanf("%[^\n]" ,&str);
+	int i = 0; 
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			printf("%c", str[i]); 
+		}
+		i++; 
+	}
+	
+
+}
